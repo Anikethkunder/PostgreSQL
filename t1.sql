1e71503489accc21e01d71c0a683e5f763208de0
Anikethkunder <116185191+Anikethkunder@users.noreply.github.com>
2025-01-24 10:30:52 +0530

    Add files via upload

---
+++ t1.sql
@@ -0,0 +1,21 @@
+"moon_id","name","planet_id","size","has_atmosphere"
+1,"Moon",1,"0.2724",False
+2,"Phobos",2,"0.0005",False
+3,"Deimos",2,"0.0003",False
+4,"Io",3,"0.286",True
+5,"Europa",3,"0.245",True
+6,"Ganymede",3,"0.413",True
+7,"Callisto",3,"0.378",True
+8,"Titan",3,"0.404",True
+9,"Venus No Moon",4,"0.0",False
+10,"Proxima b Moon",5,"0.1",False
+11,"Sirius b Satellite",6,"0.2",False
+12,"Betelgeuse No Moon",7,"0.0",False
+13,"Rigel a Moon",8,"0.5",True
+14,"Rigel b Moon",8,"0.3",True
+15,"Vega a Moon",9,"0.6",True
+16,"Altair b Moon",10,"0.7",True
+17,"Alpha No Moon",11,"0.0",False
+18,"Beta Small Moon",12,"0.2",True
+19,"Beta Large Moon",12,"0.4",True
+20,"Beta Hidden Moon",12,"0.1",False
